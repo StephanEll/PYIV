3288f10e236f73b6dc9079499618aa708d268689
Sergio85 <holzmann.sergej@gmail.com>
2014-05-10 09:47:23 +0200

    RegisterView
    StartView und RegisterView hinzugefügt und benötigte Klassen erstellt.
    
    Der Look ist nur zum Testen und besprechen.
    
    https://app.asana.com/0/11457191882923/11833582189723

--- Menu/RegisterView.cs
+++ Menu/RegisterView.cs
@@ -5,11 +5,44 @@ using System.Collections;
 namespace PYIV.Menu
 {
 	public class RegisterView : GuiView{
-		
 
-		public RegisterView() : base("Login")
+		private GameObject sprite;
+		private GameObject button;
+		private UIInput name;
+		private UIInput email;
+		private UIInput password;
+
+
+		public RegisterView() : base("RegisterPrefab")
 		{
 			
+		}
+
+		public void AddToScreen (GameObject guiParent, GameObject sceneParent) {
+			base.AddToScreen(guiParent, sceneParent);
+		}
+		
+		protected override void OnPanelCreated ()
+		{
+			base.OnPanelCreated ();
+
+			// Getting Components of View
+			sprite = panel.transform.FindChild("Sprite").gameObject;
+			button = sprite.transform.FindChild("Register_Button").gameObject;
+			name = sprite.transform.FindChild("Name_Textfield").gameObject.GetComponent<UIInput>();
+			email = sprite.transform.FindChild("Email_Textfield").gameObject.GetComponent<UIInput>();
+			password = sprite.transform.FindChild("Password_Textfield").gameObject.GetComponent<UIInput>();
+
+
+			UIEventListener.Get(button).onClick += OnClick;
+
+			
+		}
+		
+		private void OnClick(GameObject button){
+			Debug.Log("Name:" + name.value);
+			Debug.Log("Email:" + email.value);
+			Debug.Log("Password:" + password.value);
 		}
 		
 		
--- Menu/StartView.cs
+++ Menu/StartView.cs
@@ -2,11 +2,13 @@ using System;
 using UnityEngine;
 namespace PYIV.Menu
 {
-	public class ButtonView : GuiView
+	public class StartView : GuiView
 	{
-		private GameObject button;
+		private GameObject sprite;
+		private GameObject Login_Button;
+		private GameObject Register_Button;
 		
-		public ButtonView () : base("Button")
+		public StartView () : base("StartPrefab")
 		{
 			
 		}
@@ -18,9 +20,13 @@ namespace PYIV.Menu
 		protected override void OnPanelCreated ()
 		{
 			base.OnPanelCreated ();
-			button = panel.transform.FindChild("Button").gameObject;
-			UIEventListener.Get(button).onClick += OnClick;
 
+			sprite = panel.transform.FindChild("Sprite").gameObject;
+			Login_Button = sprite.transform.FindChild("Login_Button").gameObject;
+			Register_Button = sprite.transform.FindChild("Register_Button").gameObject;
+
+			UIEventListener.Get(Register_Button).onClick += OnClick;
+			
 		}
 		
 		private void OnClick(GameObject button){
