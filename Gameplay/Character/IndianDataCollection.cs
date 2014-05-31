b43c71a87b87625e130afc4950b40ba9bf90599a
Stephan Leenders <stephanleenders1.0@gmail.com>
2014-05-31 17:48:33 +0200

    Indian und alles was so dazugehört
    außer Federn

---
+++ Gameplay/Character/IndianDataCollection.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
+using PYIV.Helper;
+
+namespace PYIV.Gameplay.Character {
+
+	[XmlRoot("root")]
+	public class IndianDataCollection {
+		
+        [XmlArray("IndianDataCollection")]
+		[XmlArrayItem("IndianData", typeof(IndianData))]
+		public IndianData[] indianData { get; set; }
+
+        private static volatile IndianDataCollection instance;
+
+        private static object syncRoot = new Object();
+
+
+        private IndianDataCollection()
+        {
+            
+        }
+
+        private static IndianDataCollection DeserializeIndianDataCollection(string filename)
+        {
+
+            // Create an instance of the XmlSerializer specifying type and namespace.
+            XmlSerializer serializer = new XmlSerializer(typeof(IndianDataCollection));
+
+            // A FileStream is needed to read the XML document.
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            XmlReader reader = XmlReader.Create(fs);
+
+            // Use the Deserialize method to restore the object's state.
+            IndianDataCollection eDC = (IndianDataCollection)serializer.Deserialize(reader);
+            fs.Close();
+            return eDC;
+        }
+
+
+        public static IndianDataCollection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = DeserializeIndianDataCollection(ConfigReader.Instance.GetSetting("directory", "indian-config-xml"));
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+	}
+
+
+
+
+}
---
+++ Gameplay/Character/Weapon/ShotBehaviour/AngryBirdTestClass.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace PYIV.Gameplay.Character.Weapon {
+    public class AngryBirdTestClass : ShotBehaviour {
+
+
+        public override void EndSwipeHandler(Bullet bulletPrefab, Vector2 startPos, Vector2 endPos, float duration)
+        {
+            // implement flight behaviour here
+        }
+    
+        public override void StartSwipeHandler(Vector2 startPos)
+        {
+        
+        }
+
+        public override void UpdateSwipeHandler(Vector2 startPos, Vector2 endPos, float duration)
+        {
+
+        }
+    }
+}
