c219ceeef4ff11a24a7b1257c4561c8283d20ed9
Sergio85 <holzmann.sergej@gmail.com>
2014-07-14 16:20:31 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Menu/GameView.cs

---
+++ Menu/FlyNote.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyNote : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.up * (Time.deltaTime * 5));
+		UILabel label = gameObject.GetComponent<UILabel>();
+
+		if(label.alpha <= 0f) {
+			Debug.Log("alpha null!");
+			NGUITools.Destroy(gameObject);
+		} else {
+			label.alpha -= 0.02f;
+		}
+	}
+}
