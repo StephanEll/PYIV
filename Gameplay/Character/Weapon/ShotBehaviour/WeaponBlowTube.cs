3445f53746919c569219cac57ab65516066829f4
Stephan Leenders <stephanleenders1.0@gmail.com>
2014-09-19 15:37:47 +0200

    Anpassung BlowTube

--- Gameplay/Character/Weapon/ShotBehaviour/WeaponBlowTube.cs
+++ Gameplay/Character/Weapon/ShotBehaviour/WeaponBlowTube.cs
@@ -7,8 +7,9 @@ namespace PYIV.Gameplay.Character.Weapon
   {
 
 
-        public override void EndSwipeHandler(Bullet bullet, Vector2 startPos, Vector2 endPos, float duration)
-        {
+    public override void EndSwipeHandler(Bullet bullet, Vector2 startPos, Vector2 endPos, float duration)
+    {
+
 			bullet.transform.position = this.transform.position;
 			bullet.transform.Translate (new Vector3 (1.1f, 1.3f, 0.0f));
 			Invoke("GoToStartPosition", 0.5f);
@@ -20,14 +21,14 @@ namespace PYIV.Gameplay.Character.Weapon
       float speed = Mathf.Sqrt((delta.x * delta.x) + (delta.y * delta.y)) / duration;
 
 
-            if (delta.x < 2 || speed < 800.0f) {
+      if (delta.x < - 300 ){ //|| speed < 800.0f) {
 				//do nothing
 				Destroy(bullet.gameObject);
 			} else {
 				bullet.rigidbody2D.AddForce(delta.normalized * 1000.0f);
 			}
 
-        }
+    }
     
     public override void StartSwipeHandler(Vector2 startPos)
     {
